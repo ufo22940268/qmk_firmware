c4495ff5c2dce78f9c6739eb689fc622359161c9
Chao Cheng <ufo22940268@gmail.com>
2023-03-13 15:58:39 +0800

    v

---
+++ keyboards/soflev2/rules.mk
@@ -0,0 +1,9 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+SPLIT_KEYBOARD = yes
+DEFAULT_FOLDER = soflev2/rev1
+SWAP_HANDS_ENABLE = no
---
+++ keyboards/soflev2/sofle.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#if defined(KEYBOARD_soflev2_rev1)
+#    include "rev1.h"
+#elif defined(KEYBOARD_soflev2_keyhive)
+#    include "keyhive.h"
+#endif
